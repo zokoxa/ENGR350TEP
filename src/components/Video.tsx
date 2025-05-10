d07f9ad5d896640a93d6b1b0a3c760d4e0ed5e93
Hector Soltero <zokoxa@outlook.com>
2025-05-10 10:31:07 -0700

    test

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,18 +3,42 @@ import React, { Fragment } from "react";
 const Video = () => {
   return (
     <Fragment>
-      <h2 className="container-fluid text-center text-light">
-        What do you want to be when you grow up?
-      </h2>
-      <div className="p-0 m-0 container-fluid align-items-center justify-content-center d-flex flex-column">
-        <iframe
-          className="p-4 m-5 rounded-3 bg-dark"
-          style={{ aspectRatio: "16/9", width: "65%", overflow: "hidden" }}
-          src="https://www.youtube-nocookie.com/embed/0j0xzuh-6rY?si=JuhZdTlzZOZQyACp"
-          title="Tikkok vs Douyin"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+      <div className="container-fluid p-0 m-0">
+        <h2 className="text-center text-light mb-4">
+          What do you want to be when you grow up?
+        </h2>
+        <div className="d-flex align-items-center justify-content-center flex-wrap">
+          {/* Left image */}
+          <img
+            src="astro.avif"
+            alt="astronauts"
+            className="img-fluid mx-2 d-none d-md-block"
+            style={{ maxWidth: "15%", height: "auto" }}
+          />
+
+          {/* Video iframe container */}
+          <div
+            className="flex-shrink-1 flex-grow-1 mx-2"
+            style={{ maxWidth: "65%", width: "100%" }}
+          >
+            <iframe
+              className="w-100 rounded-3 bg-dark"
+              style={{ aspectRatio: "16/9" }}
+              src="https://www.youtube-nocookie.com/embed/0j0xzuh-6rY?si=JuhZdTlzZOZQyACp"
+              title="Tikkok vs Douyin"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+          </div>
+
+          {/* Right image */}
+          <img
+            src="pauls.png"
+            alt="pauls"
+            className="img-fluid mx-2 d-none d-md-block"
+            style={{ maxWidth: "15%", height: "auto" }}
+          />
+        </div>
       </div>
     </Fragment>
   );
